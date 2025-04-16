4f98e99e25c8f38ba206994eae279ba0819824ad
pravallika-spcl <spcl1208@gmail.com>
2025-04-16 12:35:28 +0530

    Updated test_app.py

--- tests/selenium/test_app.py
+++ tests/selenium/test_app.py
@@ -1,22 +1,25 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import time
+import traceback
 
 options = Options()
 options.add_argument("--headless")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
 
-driver = webdriver.Chrome(options=options)
-
 try:
+    driver = webdriver.Chrome(options=options)
     driver.get("http://localhost:8080/onlinebookstore/")
     time.sleep(3)  # wait for page to load
     assert "Online Bookstore" in driver.title
     print("✅ Test Passed: Title matched")
 except Exception as e:
     print(f"❌ Test Failed: {e}")
+    traceback.print_exc()
     exit(1)
 finally:
-    driver.quit()
-
+    try:
+        driver.quit()
+    except:
+        pass
