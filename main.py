ce49683b7da3f38a1b4d68dda9cb99ddbf4996a9
pravallika-spcl <spcl1208@gmail.com>
2025-05-14 17:15:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import tempfile
  
 # === Chrome Headless Setup ===
 options = webdriver.ChromeOptions()
-# options.add_argument('--headless')
+options.add_argument('--headless')
 options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
 options.add_argument('--disable-gpu')
